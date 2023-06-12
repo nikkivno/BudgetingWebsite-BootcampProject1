875fe1bc650a14e479a3597ee05fc1f24c2c5e5c
nikkivno <nikkivigneault@gmail.com>
2023-06-12 18:03:04 -0400

    connecting data to piechart, I think

--- Assets/d3api.js
+++ Assets/d3api.js
@@ -1,15 +1,15 @@
 // Pie Chart Stuff 
-const container = d3.select("#pieChart");
-const svg = container.append("svg")
-  .attr("width", 300)
-  .attr("height", 300);
+//const container = d3.select("#pieChart");
+//const svg = container.append("svg")
+  //.attr("width", 300)
+  //.attr("height", 300);
 
 // Create a circle within the SVG
-svg.append("circle")
-  .attr("cx", 150)
-  .attr("cy", 150)
-  .attr("r", 147)
-  .attr("fill", "#F2A7C3");
+//svg.append("circle")
+  //.attr("cx", 150)
+  //.attr("cy", 150)
+  //.attr("r", 147)
+  //.attr("fill", "#F2A7C3");
 
 //   Expense Input Form 
 
@@ -35,4 +35,56 @@ document.getElementById('culminate').addEventListener('click', function(){
 
 // Form to d3.js chart
 
- 
+const container = d3.select("#chart");
+const svg = container.append("svg")
+  .attr("width", 300)
+  .attr("height", 300);
+
+function createPieChart(data) {
+  // Set up dimensions for the chart
+  var width = 300;
+  var height = 300;
+  var radius = Math.min(width, height) / 2;
+
+  // Set up color scale
+  var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+  // Create SVG element
+  var chart = svg.append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+  // Generate pie chart layout
+  var pie = d3.pie()
+    .value(function(d) { return d; });
+
+  // Generate arc paths
+  var arc = d3.arc()
+    .innerRadius(0)
+    .outerRadius(radius);
+
+  // Generate pie slices
+  var slices = chart.selectAll("path")
+    .data(pie(data))
+    .enter()
+    .append("path")
+    .attr("d", arc)
+    .attr("fill", function(d, i) { return color(i); });
+}
+
+ document.getElementById('dataForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+  
+  // Get form values
+  let data1 = parseFloat(document.getElementById('data1').value);
+  let data2 = parseFloat(document.getElementById('data2').value);
+  let data3 = parseFloat(document.getElementById('data3').value);
+  let data4 = parseFloat(document.getElementById('data4').value);
+  let data5 = parseFloat(document.getElementById('data5').value);
+  
+  let data = [data1, data2, data3, data4, data5]; // Store form data in an array
+  
+  svg.selectAll("*").remove();
+
+  // Call a function to create the pie chart using the data
+  createPieChart(data);
+});
